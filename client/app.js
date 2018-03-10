d4805965fa486ab6006e4e1c95611f6072291c69
Justin <jvcruz@ucdavis.edu>
2018-03-10 03:17:08 -0800

    points plotted on map

--- client/app.js
+++ client/app.js
@@ -16,12 +16,38 @@ const path = d3.geoPath()
   .projection(projection);
 
 
+
 const g = svg.append('g');
 
 d3.json('https://unpkg.com/world-atlas@1/world/110m.json', (err, topology) => {
 
   d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json', (err, mapData) => {
-    console.log(mapData);
+    const features = mapData.features.filter(d => !!d.geometry && !!d.properties.mass);
+
+    features.forEach(d => d.properties.mass = Number(d.properties.mass));
+
+    const mExtent = d3.extent(features, d => d.properties.mass);
+    console.log(mExtent);
+
+    const mScale = d3.scaleLinear()
+      .domain(mExtent)
+      .range([1, 40, 50]);
+    const cScale = d3.scaleLinear()
+      .domain(mExtent)
+      .range(['blue', 'red']);
+
+    g.selectAll('circle')
+      .data(features)
+      .enter()
+      .append('circle')
+      .attr('cx', d => {
+        return projection(d.geometry.coordinates)[0]
+      })
+      .attr('cy', d => {
+        return projection(d.geometry.coordinates)[1]
+      })
+      .attr('r', d => mScale(d.properties.mass))
+      .attr('fill', d => cScale(d.properties.mass));
   });
 
 	g.selectAll('path')
